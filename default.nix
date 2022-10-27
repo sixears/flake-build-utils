3c8a2d4f3e783f12fe412b671f8873fe93f59a55
Mr. Fluffy <git@sixearsm>
2022-10-27 07:34:18 +0000

    add support for unbreaking packages & choosing a GHC

--- default.nix
+++ default.nix
@@ -4,19 +4,47 @@ rec {
   pcks = system: xs:
     builtins.mapAttrs (pname: p: p.defaultPackage.${system}) xs;
 
-  hPackage = system: pkgs: name: self: deps:
+  hPackage = system: pkgs: name: self: opts:
     let
-      haskellPackages = pkgs.legacyPackages.${system}.haskellPackages;
+      # haskellPackages = pkgs.legacyPackages.${system}.haskellPackages;
+      # haskellLib      = pkgs.legacyPackages.${system}.haskell.lib;
+      haskellPackages = pkgs.haskellPackages;
+      haskellLib      = pkgs.haskell.lib;
+      default_unbreak = _: {};
+      unbreak         = opts.unbreak or default_unbreak;
+      unbroken        = builtins.mapAttrs (_: p: haskellLib.markUnbroken p)
+                                          (unbreak haskellPackages);
+      d = (pcks system (opts.deps or {})) // unbroken;
     in
-      haskellPackages.callCabal2nix name self (pcks system deps);
+      builtins.trace d (haskellPackages.callCabal2nix name self d);
 
-
-  hOutputs = self: nixpkgs: packageName: deps:
-        flake-utils.lib.eachDefaultSystem (system:
+  hOutputs = self: nixpkgs_: packageName: opts:
+    flake-utils.lib.eachDefaultSystem (system:
       let
-        haskellPackages = nixpkgs.legacyPackages.${system}.haskellPackages;
+        trace2    = nixpkgs_.lib.debug.traceSeqN 2;
+        chooseGHC = _final: prev:
+          let
+            ghcNames  = with builtins; filter (x: "ghc" == substring 0 3 x)
+                                              (attrNames prev.haskell.packages);
+            traceGHCs = trace2 { "available haskell packages" = ghcNames; };
+            pickGHC   = opts.ghc or (p: p.ghcHEAD); # choose HEAD if no opts.ghc
+          in
+            {
+              haskellPackages = traceGHCs pickGHC prev.haskell.packages;
+            };
+        nixpkgs = import nixpkgs_ { system = system;
+                                    overlays = [ chooseGHC ];
+                                  };
 
-        p = hPackage system nixpkgs "base0t" self deps;
+        # haskellPackages = nixpkgs.legacyPackages.${system}.haskellPackages;
+        haskellPackages =
+          let
+            ghcDeriv =  nixpkgs.haskellPackages.ghc;
+            traceGHC = trace2  { "using ghc" = builtins.toString ghcDeriv; };
+          in
+            traceGHC nixpkgs.haskellPackages;
+
+        p = hPackage system nixpkgs "base0t" self opts;
       in
     {
       packages.${packageName} = p;
