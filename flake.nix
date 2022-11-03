ca492c76886ed071ca21de876aaa6f9a67fa39cf
Mr. Fluffy <git@sixearsm>
2022-11-03 05:48:27 +0000

    base0->0.0.4.2;base0t->0.0.1.2

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "flake build utility functions";
   inputs = {
-    base0.url       = "github:sixears/base0/r0.0.4.1";
+    base0.url       = "github:sixears/base0/r0.0.4.2";
     flake-utils.url = "github:numtide/flake-utils/c0e246b9";
   };
   outputs = { self, base0, flake-utils }: {
